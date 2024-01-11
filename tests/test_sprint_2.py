7d902c6ae6c735f5600f01e42bf94b1511e7f000
grecisuazo <grecisuazo@gmail.com>
2024-01-11 15:33:11 -0600

    Prueba comentario

--- tests/test_sprint_2.py
+++ tests/test_sprint_2.py
@@ -15,6 +15,7 @@ _JSON_PATH = os.path.join(pathlib.Path(__file__).parent.parent, "locators", "Hom
 # --------------------------------------------JUAN LARIOS---------------------------------------------------------------
 # HOME PAGE-------------------------------------------------------------------------------------------------------------
 # MXTEST-9075
+# PRUEBA
 @pytest.mark.sprint2_regression
 @pytest.mark.homepages2
 @pytest.mark.flaky(reruns=2)
